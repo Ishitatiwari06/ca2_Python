b7bdb49632b22c72ba3fed2b254a7a1e942b0543
Ishitatiwari06 <148783165+Ishitatiwari06@users.noreply.github.com>
2025-04-04 23:19:27 +0530

    Update ca2.py

--- ca2.py
+++ ca2.py
@@ -1,5 +1,7 @@
-#Preprocessing
+#Data Preprocessing & Cleaning
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 # #load dataset
 sales_data=pd.read_csv("C:\\Users\\ishit\\python\\numpy_pandas\\ca2\\Candy_Sales.csv")
 #  #1. Convert date columns to datetime
@@ -14,8 +16,16 @@ sales_data.drop_duplicates(inplace=True)
 # #4. Dataset Summary
 # print(sales_data.info())
 # print(sales_data.head())
-
-# obj1
+#EDA
+#1. Summary Statistics
+# print("Summary stats-")
+# print(sales_data.describe())
+#2. Correlation & Covariance
+# print("Correlation Matrix")
+# print(sales_data[["Sales","Units","Gross Profit","Cost"]].corr())
+# print("Covariance Matrix")
+# print(sales_data[["Sales","Units","Gross Profit","Cost"]].cov())
+# # obj1
 #1. calculate the total revenue, average revenue per order, and standard deviation of sales from the Candy_Sales dataset.
 # import numpy as np
 # import pandas as pd
@@ -41,9 +51,6 @@ sales_data.drop_duplicates(inplace=True)
 # print("State with the highest Sales:",state_highest_sale)
 
 #obj3-- "Analyzing Sales Performance and Profitability Trends of Candy Products Using Data Visualization Techniques"
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
 # 1. sales trends over time
 # plt.figure(figsize=(10,6))
 # sns.lineplot(data=sales_data,x="Order Date",y="Sales")
@@ -64,20 +71,13 @@ import seaborn as sns
 # plt.grid(axis="y",linestyle="--")
 # plt.show()
 #3. Analyze the distribution of sales and costs
-# plt.figure(figsize=(12,5))
-# sns.histplot(sales_data["Sales"],kde=True,bins=30,color="b",label="Sales")
-# sns.histplot(sales_data["Cost"],kde=True,bins=30,color="r",label="Cost")
+# plt.figure(figsize=(8,5))
+# sns.histplot(sales_data["Sales"],bins=30,color="b",label="Sales")
+# sns.histplot(sales_data["Cost"],bins=30,color="r",label="Cost")
 # plt.title("Distribution of Sales and Costs")
 # plt.xlabel("Metric")
 # plt.ylabel("Amount")
 # plt.legend()
-# plt.show()
-# melted_data = sales_data.melt(value_vars=["Sales", "Cost"], var_name="Metric", value_name="Amount")
-# plt.figure(figsize=(8,6))
-# sns.boxplot(data=melted_data,x="Metric",y="Amount")
-# plt.title("Sales vs Costs Distribution")
-# plt.xlabel("Metric")
-# plt.ylabel("Amount")
 # plt.show()
 #4. identify correlations between sales, cost, and profit
 # data=sales_data[["Sales","Cost","Gross Profit"]]
@@ -86,3 +86,27 @@ import seaborn as sns
 # plt.title("Correlation Heatmap")
 # plt.show()
 
+#obj4
+# detect and visualize outliers in the Sales and Gross Profit 
+# def detect_outliers(data,col):
+#     Q1=data[col].quantile(0.25)
+#     Q3=data[col].quantile(0.75)
+#     IQR=Q3-Q1
+#     lower_bound=Q1-1.5*IQR
+#     upper_bound=Q3+1.5*IQR
+#     outliers=data[(data[col]<lower_bound) | (data[col]>upper_bound)]
+#     return outliers
+# sales_outlier=detect_outliers(sales_data,"Sales")
+# gross_profit_outlier=detect_outliers(sales_data,"Gross Profit")
+# print("Outliers in Sales:",sales_outlier)
+# print("Outliers in Gross Profit:",gross_profit_outlier)
+# #box plot
+# plt.figure(figsize=(10,5))
+# plt.subplot(1,2,1)
+# sns.boxplot(sales_data["Sales"],color="skyblue")
+# plt.title("Box Plot of Sales")
+# plt.subplot(1,2,2)
+# sns.boxplot(sales_data["Gross Profit"],color="lightcoral")
+# plt.title("Box Plot of Gross Profit")
+# plt.tight_layout()
+# plt.show()
